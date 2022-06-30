7cff019e99a673a9e9b6f92de2f861e817ddf258
jjhurtadoa <jjhurtadoa@unal.edu.co>
2022-06-30 09:41:37 -0500

    pregunta4

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+        for line in sys.stdin:
+                letter, date, val = line.strip().split()
+                sys.stdout.write("{}\t1\n".format(letter))
